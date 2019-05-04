2f59a9da70fd1f766e7a1f0878a27a679ad10ac3
Atif Mahmud <atifmahmud101@gmail.com>
2019-05-04 14:52:35 -0400

    Update photo size

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const app = document.getElementById('root');
 
 const logo = document.createElement('img');
 
-logo.src = 'https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/1200px-Studio_Ghibli_logo.svg.png';
+logo.src = 'https://upload.wikimedia.org/wikipedia/sco/thumb/c/ca/Studio_Ghibli_logo.svg/800px-Studio_Ghibli_logo.svg.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
